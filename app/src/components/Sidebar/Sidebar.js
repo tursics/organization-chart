40bd669ef635e41721e47337f6e892d4b7317e66
Thomas Tursics <thomas@tursics.de>
2025-11-11 23:48:44 +0100

    prepare presentation mode

--- app/src/components/Sidebar/Sidebar.js
+++ app/src/components/Sidebar/Sidebar.js
@@ -65,7 +65,9 @@ const Sidebar = forwardRef(
       if (selected === "document") {
         setActiveTap("document");
       } else if (selected != null && selected.id) {
-        setActiveTap("organisation");
+        if (activeTap !== "presentation") {
+          setActiveTap("organisation");
+        }
       } else {
         setActiveTap(null);
       }
@@ -289,6 +291,37 @@ const Sidebar = forwardRef(
                   >
                     <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
                   </svg>
+                </Button>
+              </ButtonGroup>
+            </Nav>
+            <Nav className="me-auto" style={{display: 'none'}}>
+              <ButtonGroup className="view-group">
+                <Button
+                  variant="light"
+                  className={
+                    "mode-edit-toolbar-item" +
+                      (activeTap !== "presentation" ? " active" : "")
+                  }
+                  onClick={() => {
+                    setActiveTap(null);
+                  }}
+                  title="Wechsel zum Bearbeiten-Modus"
+                >
+                  Bearbeiten
+                </Button>
+                <Button
+                  variant="light"
+                  className={
+                    "mode-presentation-toolbar-item" +
+                      (activeTap === "presentation" ? " active" : "")
+                  }
+                  onClick={() => {
+                    setActiveTap('presentation');
+                    setInfoModalShow(false);
+                  }}
+                  title="Wechsel zum Präsentieren-Modus"
+                >
+                  Präsentieren
                 </Button>
               </ButtonGroup>
             </Nav>
